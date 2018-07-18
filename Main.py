8755da1741dffaedc7311f79fedf85492465cff8
YJ <songyj63@gmail.com>
2018-07-18 22:36:39 +0900

    NeuralNet MNIST example

--- Main.py
+++ Main.py
@@ -1,4 +1,8 @@
+import numpy as np
 import sys, os
+from dataset.mnist import load_mnist
+from PIL import Image
+import NeuralNet_Mnist_Eaxmple as nme
 
 
 # print(Perceptron.XOR(0,0))
@@ -30,15 +34,53 @@ import sys, os
 # a = np.array([0.3, 2.9, 4.0])
 # print(af.softmax_function(a))
 
-# MNIST data load and draw
-sys.path.append(os.pardir)
-from dataset.mnist import load_mnist
+# # MNIST data load and draw
+# sys.path.append(os.pardir)
+#
+# (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+# print(x_train.shape)
+# print(t_train.shape)
+# print(x_test.shape)
+# print(t_test.shape)
+#
+# def img_show(img):
+#     pil_img = Image.fromarray(np.uint8(img))
+#     pil_img.show()
+#
+# img = x_train[0]
+# label = t_train[0]
+# print(label)
+#
+# print(img.shape)
+# img = img.reshape(28, 28)
+# print(img.shape)
+#
+# img_show(img)
 
-(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+# # MNIST example, predict & accuracy
+# x, t = nme.get_data()
+# network = nme.init_network()
+#
+# accuracy_cnt = 0
+# for i in range(len(x)):
+#     y = nme.predict(network, x[i])
+#     p = np.argmax(y)    # 확률이 가장 높은 원소의 인덱스를 얻는다
+#     if p == t[i]:
+#         accuracy_cnt += 1
+#
+# print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
 
-print(x_train.shape)
-print(t_train.shape)
-print(x_test.shape)
-print(t_test.shape)
+# MNIST example, predict & accuracy using batch
+x, t = nme.get_data()
+network = nme.init_network()
 
-from PIL import Image
+batch_size = 100
+accuracy_cnt = 0
+
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = nme.predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1)
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
